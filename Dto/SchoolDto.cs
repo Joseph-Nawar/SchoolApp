f39947452d38a028c096957aa2dd73d1b1bbb9de
Joseph-Nawar <112328118+Joseph-Nawar@users.noreply.github.com>
2024-08-07 10:05:29 +0300

    Add files via upload

---
+++ Dto/SchoolDto.cs
@@ -0,0 +1,11 @@
+﻿using SchoolApp.Models;
+
+namespace SchoolApp.Dto
+{
+    public class SchoolDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
