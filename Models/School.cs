5000e91ec22a2e7e103836b12ac28eb077200025
Joseph-Nawar <112328118+Joseph-Nawar@users.noreply.github.com>
2024-08-07 10:08:09 +0300

    Add files via upload

---
+++ Models/School.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolApp.Models
+{
+    public class School
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public ICollection<Student> Students { get; set; }
+        public ICollection<Teacher> Teachers { get; set; }
+
+    }
+}
