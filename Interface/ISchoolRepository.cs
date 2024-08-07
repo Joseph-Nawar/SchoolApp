a2047231677097d0437dd3ca029245f312b128d0
Joseph-Nawar <112328118+Joseph-Nawar@users.noreply.github.com>
2024-08-07 10:07:37 +0300

    Add files via upload

---
+++ Interface/ISchoolRepository.cs
@@ -0,0 +1,21 @@
+﻿using SchoolApp.Models;
+using System.Diagnostics.Metrics;
+
+namespace SchoolApp.Interface
+{
+    public interface ISchoolRepository
+    {
+        ICollection<School> GetSchools();
+        School GetSchool(int id);
+        bool SchoolExists(int id);
+        bool CreateSchool(School school);
+        bool UpdateSchool(School school);
+        bool DeleteSchool(School school);
+        bool Save();
+
+        School GetSchoolByTeacher(int teacherId);
+        ICollection<Teacher> GetTeachersFromSchool(int schoolId);
+        School GetSchoolByStudent(int studentId);
+        ICollection<Student> GetStudentsFromSchool(int schoolId);
+    }
+}
